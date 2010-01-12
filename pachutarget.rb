80a46abc13ec86f2424950b12c7c543e8a85423e
Kit Plummer <kplummer@blitz.(none)>
2010-01-11 17:10:52 -0700

    adding a bit of protection around request.host

--- pachutarget.rb
+++ pachutarget.rb
@@ -9,20 +9,22 @@ get '/' do
 end
 
 post '/api/v1' do
-  body = JSON.parse(params[:body])
+  if (request.host == "pachube.com" || request.host == "example.org") 
+    body = JSON.parse(params[:body])
 
-  Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
-  Net::SMTP.start('smtp.gmail.com', 587, 'gmail.com', 'pachutarget@gmail.com', 'foobar99', :login) do |smtp|
-    smtp.open_message_stream('pachutarget@gmail.com', 'kitplummer@gmail.com') do |f|
-      f.puts 'From: pachutarget@gmail.com'
-      f.puts 'To: kitplummer@gmail.com'
-      f.puts "Subject: #{body['environment']['title']} exceeded a threshold."
-      f.puts "Date: #{Time.now.rfc2822}" 
-      f.puts
-      f.puts "#{body['environment']['title']} #{body['environment']['feed']} exceeded a threshold."
-      f.puts "Time reported: #{body['timestamp']}"
-      f.puts "Threshold: #{body['threshold_value']}"
-      f.puts "Reported Value: #{body['triggering_datastream']['value']['current_value']}"
+    Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+    Net::SMTP.start('smtp.gmail.com', 587, 'gmail.com', 'pachutarget@gmail.com', 'foobar99', :login) do |smtp|
+      smtp.open_message_stream('pachutarget@gmail.com', 'kitplummer@gmail.com') do |f|
+        f.puts 'From: pachutarget@gmail.com'
+        f.puts 'To: kitplummer@gmail.com'
+        f.puts "Subject: #{body['environment']['title']} exceeded a threshold."
+        f.puts "Date: #{Time.now.rfc2822}" 
+        f.puts
+        f.puts "#{body['environment']['title']} #{body['environment']['feed']} exceeded a threshold."
+        f.puts "Time reported: #{body['timestamp']}"
+        f.puts "Threshold: #{body['threshold_value']}"
+        f.puts "Reported Value: #{body['triggering_datastream']['value']['current_value']}"
+      end
     end
   end
 end
